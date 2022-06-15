2517355dfdf518bdb9b8329230c53a8a7a5282f7
Loris Mario S. GOMEZ <mario.gomez@sbin.bj>
2022-06-15 11:21:20 +0100

    feat: Spring boot frontend

---
+++ 01-securing-your-first-application/java/springboot/frontend/src/main/java/bj/sbin/frontend/ItemsController.java
@@ -0,0 +1,27 @@
+package bj.sbin.frontend;
+
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemsController {
+
+  @GetMapping("/items")
+  public String getItems(ModelMap modelMap) {
+    modelMap.put("items", Arrays.asList("Ordinateur", "Téléphone", "Lunettes", "Souris"));
+    return "items";
+  }
+
+  @GetMapping("/logout")
+  public String logout(HttpServletRequest request) throws ServletException {
+    request.logout();
+    
+    return "redirect:/";
+  }
+}
